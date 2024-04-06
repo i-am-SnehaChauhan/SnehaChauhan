3f15c6da88b5adc92f008f482254274bd26a303e
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-04-06 12:46:52 +0530

    Minor changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,7 +20,7 @@ export const Footer = () => {
               <a href="https://github.com/i-am-SnehaChauhan"><img src={navIcon2} alt="Icon" /></a>
               <a href="https://twitter.com/_Sneha_Chauhan"><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2024. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
