45494895e10444e7545c19bf2e1fc344b7e45434
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-04-06 17:27:56 +0530

    Analytics added

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 import { Experience } from "./components/Experience";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="App">
+      <Analytics />
       <NavBar />
       <Banner />
       <Skills />
