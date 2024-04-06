6e2573543b9dd0de6b6376bcc76725138249be8f
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-04-06 12:46:16 +0530

    Footer Added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import logo from "../assets/img/logo.png";
+import navIcon1 from "../assets/img/linkedin-in.svg";
+import navIcon2 from "../assets/img/github.svg";
+import navIcon3 from "../assets/img/x-twitter.svg";
 
 export const Footer = () => {
   return (
@@ -16,9 +16,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/i-am-snehachauhan/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/i-am-SnehaChauhan"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://twitter.com/_Sneha_Chauhan"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
