6e9bf4e9bbc908d76459b8f9152e6a3c5931a073
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-04-06 12:24:32 +0530

    Projects Added

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,26 @@
 import { Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe, faBook } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, projectUrl, githubUrl, documentationUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} />
+        <img src={imgUrl} alt='img' />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
           <div className="proj-icon">
-            
+            <a href={projectUrl} target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGlobe}  className="icon"/>
+            </a>
+            <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub}  className="icon"/>
+            </a>
+            <a href={documentationUrl} target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faBook}  className="icon"/>
+            </a>
           </div>
         </div>
       </div>
